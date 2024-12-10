8a6b8acaefab8119503d8a047a3072f7ba24f560
adit-cihuy <mrslow321@gmail.com>
2024-12-11 02:09:43 +0700

    sekian

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import BestSeller from "../components/BestSeller";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import OurPolicy from "../components/OurPolicy";
-import NewsLetterBox from "D:\lokamartfinal\frontend\src\components\NewsLetterBox.jsx";
+import NewsLetterBox from "../components/NewsletterBox";
 
 
 export default function Home() {
