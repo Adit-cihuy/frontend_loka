36e8d10e08ef52576fdf8eed297df04381952b09
adit-cihuy <mrslow321@gmail.com>
2024-12-11 02:18:49 +0700

    perbaikan newsletterbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import BestSeller from "../components/BestSeller";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import OurPolicy from "../components/OurPolicy";
-import NewsLetterBox from "../components/NewsletterBox";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 
 export default function Home() {
